f3f28536701a0199da12ee9e3569e440970795ef
Yaroslav Yatsyk <31794068+yaroslavyatsyk@users.noreply.github.com>
2024-08-19 21:40:05 -0400

    minor changes

--- Data/Migrations/20240820013505_ToDo Update.Designer.cs
+++ Data/Migrations/20240820013505_ToDo Update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDo_Web_App.Data;
 
@@ -11,9 +12,11 @@ using ToDo_Web_App.Data;
 namespace ToDo_Web_App.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240820013505_ToDo Update")]
+    partial class ToDoUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20240820013505_ToDo Update.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo_Web_App.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ToDoUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignment_AspNetUsers_UserId",
+                table: "Assignment");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Assignment",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignment_AspNetUsers_UserId",
+                table: "Assignment",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignment_AspNetUsers_UserId",
+                table: "Assignment");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Assignment",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignment_AspNetUsers_UserId",
+                table: "Assignment",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
